649936528b1d2b58620e535b6f66aaa31c7fab7b
HassanGazder <hassan2110c@aptechgdn.net>
2025-10-19 16:13:00 +0500

    Sort Functionality Added

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 let app = new Vue({
-    el:'#app',
-    data:{
-        message:'hello frooooom vue',
-        allProducts:afterSchoolSubjects,  
-        cart:[],
-      
-        //    addToCart:function(products){
-        //     this.cart.push(products)
-        //    }
+        el: '#app',
+        data: {
+          allProducts: afterSchoolSubjects,
+          cart: [],
+          sortOption: '',
         },
-      methods:{
-           addToCart:function(products){
-            this.cart.push(products)
-            console.log(products.Location);
-           }
-    }
-})
+        methods: {
+          addToCart: function(product) {
+            this.cart.push(product);
+            product.Spaces--;
+            console.log("Added:", product.Location);
+            console.log("Added:", product.Spaces);
+          },
+        },
+        computed: {
+          sortedProducts: function() {
+            let sortedArray = this.allProducts.slice();
+
+            if (this.sortOption === 'Price') {
+              sortedArray.sort((a, b) => a.Price - b.Price);
+            } else if (this.sortOption === 'Availability' || this.sortOption === 'Spaces') {
+              sortedArray.sort((a, b) => a.Spaces - b.Spaces);
+            } else if (this.sortOption === 'Subject') {
+              sortedArray.sort((a, b) => a.Subject.localeCompare(b.Subject));
+            } else if (this.sortOption === 'Location') {
+              sortedArray.sort((a, b) => a.Location.localeCompare(b.Location));
+            }
+
+            return sortedArray;
+          }
+        }
+      });
--- products.js
+++ products.js
@@ -4,69 +4,79 @@ let afterSchoolSubjects = [
     Subject: "Math",
     Location: "London",
     Price: 100,
-    Spaces: 10
+    Spaces: 10,
+    Image: "assets/images/math.jpg"
   },
   {
     id: 2,
     Subject: "Physics",
     Location: "Manchester",
     Price: 120,
-    Spaces: 8
+    Spaces: 8,
+    Image: "assets/images/physics.webp"
   },
   {
     id: 3,
     Subject: "Chemistry",
     Location: "Birmingham",
     Price: 90,
-    Spaces: 12
+    Spaces: 12,
+    Image: "assets/images/chemistry.webp"
   },
   {
     id: 4,
     Subject: "English",
     Location: "Liverpool",
     Price: 80,
-    Spaces: 9
+    Spaces: 9,
+    Image: "assets/images/english.webp"
   },
   {
     id: 5,
     Subject: "Biology",
     Location: "Leeds",
     Price: 110,
-    Spaces: 7
+    Spaces: 7,
+    Image: "assets/images/bio.png"
   },
   {
     id: 6,
     Subject: "History",
     Location: "Bristol",
     Price: 95,
-    Spaces: 11
+    Spaces: 11,
+    Image: "assets/images/history.webp"
   },
   {
     id: 7,
     Subject: "Geography",
     Location: "Oxford",
     Price: 85,
-    Spaces: 10
+    Spaces: 10,
+    Image: "assets/images/geography.jpg"
   },
   {
     id: 8,
     Subject: "Computer Science",
     Location: "Cambridge",
     Price: 150,
-    Spaces: 6
+    Spaces: 6,
+    Image: "assets/images/computerscience.jpg"
   },
   {
     id: 9,
     Subject: "Art",
     Location: "Brighton",
     Price: 70,
-    Spaces: 10
+    Spaces: 10,
+    Image: "assets/images/art.webp"
   },
   {
     id: 10,
     Subject: "Music",
     Location: "Newcastle",
     Price: 100,
-    Spaces: 8
+    Spaces: 8,
+    Image: "assets/images/music.jpg"
   }
 ];
